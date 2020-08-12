169a3eba929232b9e25d8788fbbe3b482edf3a69
Andre Madarang <andre_madarang@hotmail.com>
2020-08-11 22:23:52 -0400

    Update links

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
     </div> <!-- end hero -->
 
     <div class="container-inner mx-auto">
-      <p class="text-lg sm:text-xl">Find the <a href="https://github.com">GitHub repo here</a>. This is based on the Gridsome version that can be <a href="https://github.com/drehimself/gridsome-portfolio-starter">found here</a>. This theme was forked from my <a href="https://andremadarang.com">personal website</a>. I added more features and decided to release it as an open source project. Feel free to use it for your own site or projects! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, repellat. Dolores iure, quas perspiciatis rerum quaerat atque eum repellat autem.</p>
+      <p class="text-lg sm:text-xl">Find the <a href="https://github.com/drehimself/nuxt-portfolio-starter">GitHub repo here</a>. This is based on the Gridsome version that can be <a href="https://github.com/drehimself/gridsome-portfolio-starter">found here</a>. This theme was forked from my <a href="https://andremadarang.com">personal website</a>. I added more features and decided to release it as an open source project. Feel free to use it for your own site or projects! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, repellat. Dolores iure, quas perspiciatis rerum quaerat atque eum repellat autem.</p>
 
       <div class="flex justify-between items-center py-6">
         <div class="w-full sm:w-1/2 px-8 py-8 sm:py-0">
